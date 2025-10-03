a672a02d656351149d80f070d52561a42945ad34
seanholmes05 <152421094+seanholmes05@users.noreply.github.com>
2025-10-03 15:07:28 -0400

    Assignment 6

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+class Calculator():
+    def add(self, x,y):
+        return x + y
+    
+    def subtract(self,x,y):
+        return x - y
+    
+    def multiply(self, x, y):
+        return x * y
+    def divide(self, x,y):
+        if y == 0:
+            raise ValueError("Cannot Divide by 0")
+        return x / y
+    
